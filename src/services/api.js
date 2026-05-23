d9ab6f4142cfc231c38052a169df6f9c27c84d88
AKlq12 <afif.khaliq@gmail.com>
2026-05-23 23:16:25 +0700

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
+// TODO: Ganti URL di bawah dengan URL Cloud Run backend Anda setelah di-deploy
+const API_USER_ORDER_URL = 'http://localhost:3001'; // Contoh: 'https://service-user-order-xxxx-et.a.run.app'
+const API_WORKER_SERVICE_URL = 'http://localhost:3002'; // Contoh: 'https://service-worker-xxxx-et.a.run.app'
+
 export const apiUserOrder = axios.create({
-  baseURL: import.meta.env.VITE_API_USER_ORDER,
+  baseURL: API_USER_ORDER_URL,
 });
 
 export const apiWorkerService = axios.create({
-  baseURL: import.meta.env.VITE_API_WORKER_SERVICE,
+  baseURL: API_WORKER_SERVICE_URL,
 });
