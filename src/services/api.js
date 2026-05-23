475a706d78fa3518287cdd1a5f236e48134f7961
Apip <143242286+AKlq12@users.noreply.github.com>
2026-05-24 00:19:10 +0700

    Update API URLs to production endpoints

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // TODO: Ganti URL di bawah dengan URL Cloud Run backend Anda setelah di-deploy
-const API_USER_ORDER_URL = 'http://localhost:3001'; // Contoh: 'https://service-user-order-xxxx-et.a.run.app'
-const API_WORKER_SERVICE_URL = 'http://localhost:3002'; // Contoh: 'https://service-worker-xxxx-et.a.run.app'
+const API_USER_ORDER_URL = 'https://be-user-cleanco-739468618342.us-central1.run.app'; // Contoh: 'https://service-user-order-xxxx-et.a.run.app'
+const API_WORKER_SERVICE_URL = 'https://be-admin-cleanco-739468618342.us-central1.run.app'; // Contoh: 'https://service-worker-xxxx-et.a.run.app'
 
 export const apiUserOrder = axios.create({
   baseURL: API_USER_ORDER_URL,
