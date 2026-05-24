c0e3e536df73d8cf8b15890f6c14d3df41ffde25
Apip <143242286+AKlq12@users.noreply.github.com>
2026-05-24 22:32:52 +0700

    Update Firestore project ID in configuration

--- config/firestore.js
+++ config/firestore.js
@@ -20,7 +20,7 @@ try {
 
   admin.initializeApp({
     credential: credential,
-    projectId: process.env.PROJECT_ID || 'cleanco-739468618342'
+    projectId: process.env.PROJECT_ID || 'd-08-497210'
   });
   console.log("Firestore berhasil terkoneksi!");
 } catch (error) {
