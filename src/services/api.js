1c4b6f3581e1f8a9454b6efd84346cd01a612b3c
Apip <143242286+AKlq12@users.noreply.github.com>
2026-05-24 15:41:11 +0700

    Update worker service URL for API

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Ganti URL di bawah dengan URL Cloud Run backend Anda
 const API_USER_ORDER_URL = 'https://be-user-cleanco-739468618342.us-central1.run.app';
-const API_WORKER_SERVICE_URL = 'https://be-worker-cleanco-739468618342.us-central1.run.app';
+const API_WORKER_SERVICE_URL = 'https://be-admin-cleanco-739468618342.us-central1.run.app';
 
 export const apiUserOrder = axios.create({
   baseURL: `${API_USER_ORDER_URL}/api/v1`,
